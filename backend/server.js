b7b2c4938dab8319b481a5c2839ff914e46932bf
Bramhananda K L <149486769+KLBramhananda@users.noreply.github.com>
2025-01-31 21:12:00 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://mini-link-management-platform-frontend-deploy.vercel.app",
+    origin: "https://mini-link-management-platform-front.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "user-id"],
   })
