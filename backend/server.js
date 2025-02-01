5ea8d34d87cf6f6db871b522fe0faa53be2f7b78
Bramhananda K L <149486769+KLBramhananda@users.noreply.github.com>
2025-02-01 12:35:23 +0530

    Updated myside

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3001"], //https://mini-link-management-platform-front.vercel.app
+    origin: [""],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "user-id"],
   })
