cab14fd37b7df53d076149814d993b5a337c826a
Bramhananda K L <149486769+KLBramhananda@users.noreply.github.com>
2025-02-03 21:10:11 +0530

    999

--- backend/controllers/LinkController.js
+++ backend/controllers/LinkController.js
@@ -36,7 +36,7 @@ exports.createLink = async (req, res) => {
 
     // Generate a random 6-8 character alphanumeric string
     const randomString = Math.random().toString(36).substring(2, 10);
-    const shortLink = `https://bramha-mini-link-management-back.vercel.app/${randomString}`;
+    const shortLink = `http://localhost:5000/${randomString}`;
 
     const currentDate = new Date();
     const formattedDate = currentDate.toLocaleString("en-US", {
