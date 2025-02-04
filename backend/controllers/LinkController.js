418a513ef4184801fc9a4edf048ad8192f2f9fff
Bramhananda K L <149486769+KLBramhananda@users.noreply.github.com>
2025-02-04 19:08:44 +0530

    Update 111

--- backend/controllers/LinkController.js
+++ backend/controllers/LinkController.js
@@ -37,7 +37,7 @@ exports.createLink = async (req, res) => {
     const randomString = Math.random().toString(36).substring(2, 10);
 
     //const backendURL = "http://localhost:5000" || `https://bramha-mini-link-management-back.vercel.app`;
-    const shortLink = `http://localhost:5000/${randomString}`; // just replace with your domain host : http://localhost:5000/
+    const shortLink = `https://bramha-mini-link-management-back.vercel.app/${randomString}`; // just replace with your domain host : http://localhost:5000/
 
     const currentDate = new Date();
     const formattedDate = currentDate.toLocaleString("en-US", {
